d584ffa48b5b59e620c8eb6652bf7b9595cc3a79
Amber Sharpe <amberjsharpe@gmail.com>
2018-02-01 16:33:41 -0600

    add files. set up basic html and css

---
+++ converter.js
@@ -0,0 +1,27 @@
+// Delete text in input field
+
+function eraseText() {
+  document.getElementById("clear-button").value = "";
+}
+
+document.getElementById("clear-button").addEventListener("click", eraseText());
+
+function toCelsius () {
+
+}
+
+function toFahrenheit () {
+
+}
+
+// Get a reference to the button element in the DOM
+//var button = document.getElementById("converter");
+
+// This function should determine which conversion should
+// happen based on which radio button is selected.
+//function determineConverter (clickEvent) {
+  //console.log("event", clickEvent);
+//}
+
+// Assign a function to be executed when the button is clicked
+//button.addEventListener("click", determineConverter);
