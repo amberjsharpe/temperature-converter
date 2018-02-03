f519433bf8a18b53ba5e623b58837f12d40c0c61
Amber Sharpe <amberjsharpe@gmail.com>
2018-02-02 18:00:40 -0600

    converts temperature using function

--- converter.js
+++ converter.js
@@ -1,32 +1,33 @@
 // Clear the input field using clear-input button //
 function clearInput() {
     document.getElementById("temp-input").value = "";
-  }
+}
   
-  document.getElementById("clear-input").addEventListener("click", clearInput);
+document.getElementById("clear-input").addEventListener("click", clearInput);
 
 // Submit Button //
-
-
-function calculate() {
-    var button = document.getElementById("converter");
+var button = document.getElementById("converter");
     button.addEventListener("click", determineConverter);
+
+
+function toFahrenheit(tempInput) {
+    tempInput = parseFloat(tempInput);
+    document.getElementById("temp-output").innerHTML = ((tempInput - 32) / 1.8).toFixed(2);
+}
+
+function toCelcius(tempInput) {
+    tempInput = parseFloat(tempInput);
+    document.getElementById("temp-output").innerHTML = ((tempInput * 1.8) + 32).toFixed(2);
+}
+
+function determineConverter (clickEvent) {
     var tempInput = document.getElementById("temp-input").value;
-
-    function determineConverter (clickEvent) {
-        if (document.getElementById("to-celcius").checked) {
-            function toFahrenheit(tempInput) {
-                tempInput = parseFloat(tempInput);
-                document.getElementById("temp-output").innerHTML = (tempInput - 32) / 1.8;
-            }
-        } else if (document.getElementById("to-fahrenheit").checked) {
-            function toCelcius(tempInput) {
-                tempInput = parseFloat(tempInput);
-                document.getElementById("temp-output").innerHTML = (tempInput * 1.8) + 32;
-            }
-        }
+    if (document.getElementById("to-celcius").checked) {
+        toFahrenheit(tempInput); 
+        console.log('hi')
+    } else if (document.getElementById("to-fahrenheit").checked) {
+        toCelcius(tempInput);
+        console.log("howdy");
     }
-
-}  
-  
+} 
   
